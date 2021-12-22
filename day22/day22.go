7b811c7f9a0bf71e3b7a676aa95399fd39817711
scottdharris <scottdharris11@yahoo.com>
2021-12-22 14:44:47 -0600

    day 22 puzzle

---
+++ day22/day22.go
@@ -0,0 +1,220 @@
+package day22
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"regexp"
+	"time"
+
+	"advent-of-code-2021/utils"
+)
+
+type Puzzle struct{}
+
+func (Puzzle) Solve() {
+	lines := utils.ReadLines("day22", "day-22-input.txt")
+	solvePart1(lines)
+	solvePart2(lines)
+}
+
+func solvePart1(lines []string) int {
+	cuboids := parseInput(lines)
+	start := time.Now().UnixMilli()
+	reactor := Reactor{}
+	reactor.Initialize(cuboids)
+	ans := reactor.OnCubes()
+	end := time.Now().UnixMilli()
+	log.Printf("Day 22, Part 1 (%dms): On Cubes After Init = %d", end-start, ans)
+	return ans
+}
+
+func solvePart2(lines []string) int {
+	cuboids := parseInput(lines)
+	start := time.Now().UnixMilli()
+	reactor := Reactor{}
+	reactor.Reboot(cuboids)
+	ans := reactor.OnCubes()
+	end := time.Now().UnixMilli()
+	log.Printf("Day 22, Part 2 (%dms): On Cubes After Reboot = %d", end-start, ans)
+	return ans
+}
+
+func parseInput(lines []string) []Cuboid {
+	matcher := regexp.MustCompile(`^(.+) x=(.+)\.\.(.+),y=(.+)\.\.(.+),z=(.+)\.\.(.+)$`)
+	var cuboids []Cuboid
+	for _, line := range lines {
+		groups := matcher.FindStringSubmatch(line)
+		cuboids = append(cuboids, Cuboid{
+			on:     groups[1] == "on",
+			xRange: [2]int{utils.Number(groups[2]), utils.Number(groups[3])},
+			yRange: [2]int{utils.Number(groups[4]), utils.Number(groups[5])},
+			zRange: [2]int{utils.Number(groups[6]), utils.Number(groups[7])},
+		})
+	}
+	return cuboids
+}
+
+type Reactor struct {
+	cuboids []Cuboid
+}
+
+func (r *Reactor) Cubes() int {
+	cnt := 0
+	for _, c := range r.cuboids {
+		cnt += c.Cubes()
+	}
+	return cnt
+}
+
+func (r *Reactor) OnCubes() int {
+	on := 0
+	for _, c := range r.cuboids {
+		if c.on {
+			on += c.Cubes()
+		}
+	}
+	return on
+}
+
+func (r *Reactor) Initialize(cuboids []Cuboid) {
+	for _, c := range cuboids {
+		if c.xRange[0] < -50 || c.xRange[1] > 50 ||
+			c.yRange[0] < -50 || c.yRange[1] > 50 ||
+			c.zRange[0] < -50 || c.zRange[1] > 50 {
+			return
+		}
+		r.applyCuboid(c)
+	}
+}
+
+func (r *Reactor) Reboot(cuboids []Cuboid) {
+	for _, c := range cuboids {
+		r.applyCuboid(c)
+	}
+}
+
+func (r *Reactor) applyCuboid(c Cuboid) {
+	for {
+		if done := r.handleIntersections(c); done {
+			break
+		}
+	}
+	r.cuboids = append(r.cuboids, c)
+}
+
+func (r *Reactor) handleIntersections(c Cuboid) bool {
+	for i, cuboid := range r.cuboids {
+		if !cuboid.Intersects(c) {
+			continue
+		}
+
+		if c.Overlays(cuboid) {
+			// new cuboid overlays this whole existing cube, just remove it by
+			// replacing it with the last slice entry and then trimming the slice
+			r.cuboids[i] = r.cuboids[len(r.cuboids)-1]
+			r.cuboids = r.cuboids[:len(r.cuboids)-1]
+			return false
+		}
+
+		// new cuboid intersects with this one, break into cuboids that don't intersect
+		nCuboids := make([]Cuboid, 0, len(r.cuboids)+10)
+		nCuboids = append(nCuboids, r.cuboids[0:i]...)
+		nCuboids = append(nCuboids, r.breakCuboid(cuboid, c)...)
+		nCuboids = append(nCuboids, r.cuboids[i+1:]...)
+		r.cuboids = nCuboids
+		return false
+	}
+	return true
+}
+
+func (r Reactor) breakCuboid(c Cuboid, avoid Cuboid) []Cuboid {
+	var cuboids []Cuboid
+
+	xClear := r.clearRanges(c.xRange, avoid.xRange)
+	yClear := r.clearRanges(c.yRange, avoid.yRange)
+	zClear := r.clearRanges(c.zRange, avoid.zRange)
+
+	xMin := c.xRange[0]
+	xMax := c.xRange[1]
+	for _, x := range xClear {
+		cuboids = append(cuboids, Cuboid{on: c.on, xRange: x, yRange: c.yRange, zRange: c.zRange})
+		if x[1] < xMax {
+			xMin = x[1] + 1
+		}
+		if x[0] > xMin {
+			xMax = x[0] - 1
+		}
+	}
+
+	yMin := c.yRange[0]
+	yMax := c.yRange[1]
+	for _, y := range yClear {
+		cuboids = append(cuboids, Cuboid{on: c.on, xRange: [2]int{xMin, xMax}, yRange: y, zRange: c.zRange})
+		if y[1] < yMax {
+			yMin = y[1] + 1
+		}
+		if y[0] > yMin {
+			yMax = y[0] - 1
+		}
+	}
+
+	for _, z := range zClear {
+		cuboids = append(cuboids, Cuboid{on: c.on, xRange: [2]int{xMin, xMax}, yRange: [2]int{yMin, yMax}, zRange: z})
+	}
+
+	return cuboids
+}
+
+func (Reactor) clearRanges(check [2]int, avoid [2]int) [][2]int {
+	var ranges [][2]int
+	if check[0] < avoid[0] {
+		ranges = append(ranges, [2]int{check[0], avoid[0] - 1})
+	}
+	if check[1] > avoid[1] {
+		ranges = append(ranges, [2]int{avoid[1] + 1, check[1]})
+	}
+	return ranges
+}
+
+type Cuboid struct {
+	xRange [2]int
+	yRange [2]int
+	zRange [2]int
+	on     bool
+}
+
+func (c *Cuboid) Cubes() int {
+	return c.rangeSize(c.xRange) * c.rangeSize(c.yRange) * c.rangeSize(c.zRange)
+}
+
+func (c *Cuboid) Intersects(c2 Cuboid) bool {
+	return c.rangeIntersects(c.xRange, c2.xRange) &&
+		c.rangeIntersects(c.yRange, c2.yRange) &&
+		c.rangeIntersects(c.zRange, c2.zRange)
+}
+
+func (c *Cuboid) Overlays(c2 Cuboid) bool {
+	return c.rangeOverlays(c.xRange, c2.xRange) &&
+		c.rangeOverlays(c.yRange, c2.yRange) &&
+		c.rangeOverlays(c.zRange, c2.zRange)
+}
+
+func (c *Cuboid) String() string {
+	return fmt.Sprintf("%t %d x=%d..%d, y=%d..%d, z=%d..%d",
+		c.on, c.Cubes(), c.xRange[0], c.xRange[1], c.yRange[0], c.yRange[1], c.zRange[0], c.zRange[1])
+}
+
+func (Cuboid) rangeSize(r [2]int) int {
+	return int(math.Abs(float64(r[0]-r[1]))) + 1
+}
+
+func (c Cuboid) rangeIntersects(r1 [2]int, r2 [2]int) bool {
+	return (r1[0] >= r2[0] && r1[0] <= r2[1]) ||
+		(r1[1] >= r2[0] && r1[1] <= r2[1]) ||
+		c.rangeOverlays(r1, r2)
+}
+
+func (Cuboid) rangeOverlays(r1 [2]int, r2 [2]int) bool {
+	return r1[0] <= r2[0] && r1[1] >= r2[1]
+}
---
+++ day22/day22_test.go
@@ -0,0 +1,235 @@
+package day22
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"advent-of-code-2021/utils"
+)
+
+var testValues = []string{
+	"on x=10..12,y=10..12,z=10..12",
+	"on x=11..13,y=11..13,z=11..13",
+	"off x=9..11,y=9..11,z=9..11",
+	"on x=10..10,y=10..10,z=10..10",
+}
+
+func TestSolvePart1(t *testing.T) {
+	assert.Equal(t, 39, solvePart1(testValues))
+	assert.Equal(t, 590784, solvePart1(utils.ReadLines("day22", "day-22-test.txt")))
+	assert.Equal(t, 474140, solvePart1(utils.ReadLines("day22", "day-22-test2.txt")))
+	assert.Equal(t, 655005, solvePart1(utils.ReadLines("day22", "day-22-input.txt")))
+}
+
+func TestSolvePart2(t *testing.T) {
+	assert.Equal(t, 2758514936282235, solvePart2(utils.ReadLines("day22", "day-22-test2.txt")))
+	assert.Equal(t, 1125649856443608, solvePart2(utils.ReadLines("day22", "day-22-input.txt")))
+}
+
+func TestParseInput(t *testing.T) {
+	cuboids := parseInput(testValues)
+
+	assert.Equal(t, 4, len(cuboids))
+
+	assert.True(t, cuboids[0].on)
+	assert.ElementsMatch(t, [2]int{10, 12}, cuboids[0].xRange)
+	assert.ElementsMatch(t, [2]int{10, 12}, cuboids[0].yRange)
+	assert.ElementsMatch(t, [2]int{10, 12}, cuboids[0].zRange)
+
+	assert.True(t, cuboids[0].on)
+	assert.ElementsMatch(t, [2]int{11, 13}, cuboids[1].xRange)
+	assert.ElementsMatch(t, [2]int{11, 13}, cuboids[1].yRange)
+	assert.ElementsMatch(t, [2]int{11, 13}, cuboids[1].zRange)
+
+	assert.False(t, cuboids[2].on)
+	assert.ElementsMatch(t, [2]int{9, 11}, cuboids[2].xRange)
+	assert.ElementsMatch(t, [2]int{9, 11}, cuboids[2].yRange)
+	assert.ElementsMatch(t, [2]int{9, 11}, cuboids[2].zRange)
+
+	assert.True(t, cuboids[3].on)
+	assert.ElementsMatch(t, [2]int{10, 10}, cuboids[3].xRange)
+	assert.ElementsMatch(t, [2]int{10, 10}, cuboids[3].yRange)
+	assert.ElementsMatch(t, [2]int{10, 10}, cuboids[3].zRange)
+}
+
+func TestReactor_BreakCuboid(t *testing.T) {
+	tests := []struct {
+		name     string
+		original Cuboid
+		avoid    Cuboid
+		expected []Cuboid
+	}{
+		{
+			"1",
+			Cuboid{on: true, xRange: [2]int{1, 3}, yRange: [2]int{4, 6}, zRange: [2]int{7, 9}},
+			Cuboid{on: false, xRange: [2]int{2, 4}, yRange: [2]int{3, 5}, zRange: [2]int{6, 8}},
+			[]Cuboid{
+				{on: true, xRange: [2]int{1, 1}, yRange: [2]int{4, 6}, zRange: [2]int{7, 9}},
+				{on: true, xRange: [2]int{2, 3}, yRange: [2]int{6, 6}, zRange: [2]int{7, 9}},
+				{on: true, xRange: [2]int{2, 3}, yRange: [2]int{4, 5}, zRange: [2]int{9, 9}},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert.ElementsMatch(t, tt.expected, Reactor{}.breakCuboid(tt.original, tt.avoid))
+		})
+	}
+}
+
+func TestReactor_ClearRanges(t *testing.T) {
+	tests := []struct {
+		name     string
+		check    [2]int
+		avoid    [2]int
+		expected [][2]int
+	}{
+		{"1", [2]int{1, 3}, [2]int{3, 5}, [][2]int{{1, 2}}},
+		{"2", [2]int{3, 5}, [2]int{1, 3}, [][2]int{{4, 5}}},
+		{"3", [2]int{3, 9}, [2]int{5, 7}, [][2]int{{3, 4}, {8, 9}}},
+		{"4", [2]int{3, 9}, [2]int{3, 9}, [][2]int{}},
+		{"5", [2]int{4, 8}, [2]int{3, 9}, [][2]int{}},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert.ElementsMatch(t, tt.expected, Reactor{}.clearRanges(tt.check, tt.avoid))
+		})
+	}
+}
+
+func TestCuboid_Cubes(t *testing.T) {
+	tests := []struct {
+		name     string
+		cuboid   Cuboid
+		expected int
+	}{
+		{"1", Cuboid{xRange: [2]int{10, 12}, yRange: [2]int{10, 12}, zRange: [2]int{10, 12}}, 27},
+		{"2", Cuboid{xRange: [2]int{-10, -12}, yRange: [2]int{-10, -12}, zRange: [2]int{-10, -12}}, 27},
+		{"3", Cuboid{xRange: [2]int{-1, 1}, yRange: [2]int{-1, 1}, zRange: [2]int{-1, 1}}, 27},
+		{"4", Cuboid{xRange: [2]int{11, 13}, yRange: [2]int{11, 20}, zRange: [2]int{11, 15}}, 150},
+		{"5", Cuboid{xRange: [2]int{10, 10}, yRange: [2]int{10, 10}, zRange: [2]int{10, 10}}, 1},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, tt.cuboid.Cubes())
+		})
+	}
+}
+
+func TestCuboid_IntersectsAndOverlays(t *testing.T) {
+	tests := []struct {
+		name       string
+		cuboid1    Cuboid
+		cuboid2    Cuboid
+		intersects bool
+		over12     bool
+		over21     bool
+	}{
+		{
+			"1",
+			Cuboid{xRange: [2]int{10, 12}, yRange: [2]int{10, 12}, zRange: [2]int{10, 12}},
+			Cuboid{xRange: [2]int{10, 12}, yRange: [2]int{10, 12}, zRange: [2]int{10, 12}},
+			true, true, true,
+		},
+		{
+			"2",
+			Cuboid{xRange: [2]int{10, 12}, yRange: [2]int{10, 12}, zRange: [2]int{10, 12}},
+			Cuboid{xRange: [2]int{10, 12}, yRange: [2]int{10, 12}, zRange: [2]int{-10, -12}},
+			false, false, false,
+		},
+		{
+			"3",
+			Cuboid{xRange: [2]int{10, 12}, yRange: [2]int{10, 12}, zRange: [2]int{10, 12}},
+			Cuboid{xRange: [2]int{-10, -12}, yRange: [2]int{10, 12}, zRange: [2]int{10, 12}},
+			false, false, false,
+		},
+		{
+			"4",
+			Cuboid{xRange: [2]int{10, 12}, yRange: [2]int{10, 12}, zRange: [2]int{10, 12}},
+			Cuboid{xRange: [2]int{10, 12}, yRange: [2]int{-10, -12}, zRange: [2]int{10, 12}},
+			false, false, false,
+		},
+		{
+			"5",
+			Cuboid{xRange: [2]int{10, 12}, yRange: [2]int{10, 12}, zRange: [2]int{10, 12}},
+			Cuboid{xRange: [2]int{9, 13}, yRange: [2]int{10, 12}, zRange: [2]int{10, 12}},
+			true, false, true,
+		},
+		{
+			"6",
+			Cuboid{xRange: [2]int{10, 12}, yRange: [2]int{10, 12}, zRange: [2]int{10, 12}},
+			Cuboid{xRange: [2]int{11, 11}, yRange: [2]int{10, 12}, zRange: [2]int{10, 12}},
+			true, true, false,
+		},
+		{
+			"7",
+			Cuboid{xRange: [2]int{10, 12}, yRange: [2]int{10, 12}, zRange: [2]int{10, 12}},
+			Cuboid{xRange: [2]int{11, 13}, yRange: [2]int{11, 13}, zRange: [2]int{11, 13}},
+			true, false, false,
+		},
+		{
+			"8",
+			Cuboid{xRange: [2]int{10, 12}, yRange: [2]int{10, 12}, zRange: [2]int{10, 12}},
+			Cuboid{xRange: [2]int{9, 11}, yRange: [2]int{9, 11}, zRange: [2]int{9, 11}},
+			true, false, false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.intersects, tt.cuboid1.Intersects(tt.cuboid2))
+			assert.Equal(t, tt.intersects, tt.cuboid2.Intersects(tt.cuboid1))
+			assert.Equal(t, tt.over12, tt.cuboid1.Overlays(tt.cuboid2))
+			assert.Equal(t, tt.over21, tt.cuboid2.Overlays(tt.cuboid1))
+		})
+	}
+}
+
+func TestCuboid_RangeSize(t *testing.T) {
+	tests := []struct {
+		name     string
+		iRange   [2]int
+		expected int
+	}{
+		{"1", [2]int{2, 5}, 4},
+		{"2", [2]int{-5, -2}, 4},
+		{"3", [2]int{-5, 2}, 8},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, Cuboid{}.rangeSize(tt.iRange))
+		})
+	}
+}
+
+func TestCuboid_RangeIntersectsAndOverlays(t *testing.T) {
+	tests := []struct {
+		name       string
+		range1     [2]int
+		range2     [2]int
+		intersects bool
+		over12     bool
+		over21     bool
+	}{
+		{"1", [2]int{2, 5}, [2]int{3, 6}, true, false, false},
+		{"2", [2]int{2, 5}, [2]int{-2, 3}, true, false, false},
+		{"3", [2]int{2, 5}, [2]int{6, 7}, false, false, false},
+		{"4", [2]int{2, 5}, [2]int{-2, 30}, true, false, true},
+		{"5", [2]int{2, 5}, [2]int{2, 4}, true, true, false},
+		{"6", [2]int{2, 5}, [2]int{3, 5}, true, true, false},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.intersects, Cuboid{}.rangeIntersects(tt.range1, tt.range2))
+			assert.Equal(t, tt.intersects, Cuboid{}.rangeIntersects(tt.range2, tt.range1))
+			assert.Equal(t, tt.over12, Cuboid{}.rangeOverlays(tt.range1, tt.range2))
+			assert.Equal(t, tt.over21, Cuboid{}.rangeOverlays(tt.range2, tt.range1))
+		})
+	}
+}
